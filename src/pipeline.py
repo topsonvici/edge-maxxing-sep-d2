c2b9e7630ad310aba82756af8494de8ef4ad7c23
Your Name <you@example.com>
2024-09-18 15:31:09 +0000

    stage

---
+++ src/pipeline.py
@@ -0,0 +1,35 @@
+import torch
+from PIL.Image import Image
+from diffusers import StableDiffusionXLPipeline
+from pipelines.models import TextToImageRequest
+from torch import Generator
+
+
+def load_pipeline() -> StableDiffusionXLPipeline:
+    vae = AutoencoderKL.from_pretrained(
+    'madebyollin/sdxl-vae-fp16-fix',
+    use_safetensors=True,
+    torch_dtype=torch.float16,
+    ).to('cuda')
+    pipeline = StableDiffusionXLPipeline.from_pretrained(
+        "./models/newdream-sdxl-20",
+        torch_dtype=torch.float16,
+        local_files_only=True,
+        vae=vae,
+    ).to("cuda")
+
+    pipeline(prompt="")
+
+    return pipeline
+
+
+def infer(request: TextToImageRequest, pipeline: StableDiffusionXLPipeline) -> Image:
+    generator = Generator(pipeline.device).manual_seed(request.seed) if request.seed else None
+
+    return pipeline(
+        prompt=request.prompt,
+        negative_prompt=request.negative_prompt,
+        width=request.width,
+        height=request.height,
+        generator=generator,
+    ).images[0]
